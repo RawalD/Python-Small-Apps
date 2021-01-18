09b849110363d565221cc66ab7dca6d535b058f0
rawald <diprawal@hotmail.com>
2021-01-18 08:53:23 +0200

    Ping Pong Turtle Complete

--- 35/paddle.py
+++ 35/paddle.py
@@ -3,19 +3,18 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def _init__(self, position):
+    def __init__(self, position):
         super().__init__()
-
         self.shape("square")
-        self.shapesize(stretch_wid=6, stretch_len=1)
-        self.penup()
         self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
         self.goto(position)
 
-    def move_up(self):
+    def go_up(self):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
-    def move_down(self):
+    def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
